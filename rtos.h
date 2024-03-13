bcdd05f67790d4c4b9fdb838766238d844684080
Geronimo Freese <geronimo.freese@gmail.com>
2024-03-13 13:49:00 +0100

    POSIX Compliance: Start
    added errno

--- rtos.h
+++ rtos.h
@@ -1,10 +1,14 @@
 /**
  * @file  rtos.h
- * @brief
+ * @brief Main #include file
+ *        This software is a 1 header include system
+ *
+ *        USER-INCLUDE-FILE
  */
 
 #ifndef RTOS_H
 #define RTOS_H
+
 
 
 #endif
